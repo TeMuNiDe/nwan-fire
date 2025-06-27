b196fd0ebed2610f60167d172a499bb6b7e4885e
Vihaari Varma <varma.vihaari@gmail.com>
2025-06-27 07:31:41 +0530

    Dependency cleanup

--- src/index.js
+++ src/index.js
@@ -1,58 +1,20 @@
 /* eslint-disable */
 
-import Home from './views/Home';
-import React,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from "@material-ui/core/CssBaseline";
-
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-
-
-
-const theme = createTheme({
-    
-    palette: {
-        type: 'dark',
-        primary: {
-          main: '#13A8BA',
-        },
-        secondary: {
-          main: '#13ba77',
-        },
-        background: {
-          default: '#1a2034',
-          paper: '#202940',
-        },
-      }
-  });
-
-
-  
+ 
 class App extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {user:0};
+  
     }
 
     componentDidMount(){
-        fetch(process.env.REACT_APP_SERVER_HOST,"/api/user/0")
-        .then(res=>res.json())
-        .then((result)=>{this.setState({user:result})})
-        .catch(e=>{console.log(e)});
-        
+
     }
 
     render(){
-      if(this.state.user!=0){
-       return <ThemeProvider theme={theme}>
-        <CssBaseline />
-       <Home user={this.state.user}/>
-       </ThemeProvider>;
-    } else {
-      return <Typography>Loading</Typography>;
-    }
-
+      return <p>Hello, World!</p>;
   }
 }
 ReactDOM.render(<App/>,document.getElementById('root'));
